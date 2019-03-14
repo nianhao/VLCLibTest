4734183ffc591ba3d21f6eaa9de2244acdcf5b01
nianhao <1137136542@qq.com>
2019-03-14 16:29:01 +0800

    可以打开UDP流

--- VLCLibTest/mainWindow.Designer.cs
+++ VLCLibTest/mainWindow.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel_renderWindow = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel_renderWindow
+            // 
+            this.panel_renderWindow.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel_renderWindow.Location = new System.Drawing.Point(42, 42);
+            this.panel_renderWindow.Name = "panel_renderWindow";
+            this.panel_renderWindow.Size = new System.Drawing.Size(611, 345);
+            this.panel_renderWindow.TabIndex = 0;
             // 
             // mainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel_renderWindow);
             this.Name = "mainWindow";
             this.Text = "VLCLib播放器";
             this.ResumeLayout(false);
@@ -42,6 +53,9 @@
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel_renderWindow;
     }
 }
 
--- VLCLibTest/mainWindow.cs
+++ VLCLibTest/mainWindow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,21 @@ namespace VLCLibTest
 {
     public partial class mainWindow : Form
     {
+        /// <summary>
+        /// VLC播放器
+        /// </summary>
+        public VLCBase player { get; private set; }
         public mainWindow()
         {
             InitializeComponent();
-            System
+            string PLUGIN_PATH = Environment.CurrentDirectory + "\\plugins\\";
+            Trace.WriteLine(PLUGIN_PATH);
+            player = new VLCBase(PLUGIN_PATH);
+            player.SetRenderWindow((int)panel_renderWindow.Handle);
+            player.LoadFile("udp://@192.168.1.117:9090");
+            player.Play();
         }
+
+        
     }
 }
